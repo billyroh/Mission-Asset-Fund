da2f33c1e469d96aaf95c9fd9d098e1c9588bb7b
Billy Roh <billy.roh@gmail.com>
2014-06-25 11:22:41 -0700

    Initial Commit

---
+++ Mission Asset Fund/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mission Asset Fund
+//
+//  Created by Billy Roh on 6/25/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mission Asset FundTests/Mission_Asset_FundTests.m
@@ -0,0 +1,34 @@
+//
+//  Mission_Asset_FundTests.m
+//  Mission Asset FundTests
+//
+//  Created by Billy Roh on 6/25/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mission_Asset_FundTests : XCTestCase
+
+@end
+
+@implementation Mission_Asset_FundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
